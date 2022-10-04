c054816c134ca104ae414a295e022748347001d2
Joonho Kim <idaho777@gmail.com>
2022-10-04 17:21:59 -0400

    dev

--- experiments.sh
+++ experiments.sh
@@ -1,4 +1,7 @@
-data_dir=$HOME/workspace/data/nerf-format/
+
+data_nerf_dir=$HOME/workspace/data/nerf-format/3d-proxy
+data_turn_dir=$HOME/workspace/data/nerf-format/turnarounds
+# data_dir=$HOME/workspace/data/nerf-format/mini-test
 checkpoint_dir=./_results/test/
 
 mkdir -p $checkpoint_dir
@@ -7,7 +10,7 @@ mkdir -p $checkpoint_dir
 progress_file=$checkpoint_dir/rendered
 touch $progress_file
 
-for DATA_PATH in $(find $data_dir -type d -name "*train" -exec dirname "{}" \; | sort -u); do
+for DATA_PATH in $(find $data_nerf_dir $data_turn_dir -type d -name "*train" -exec dirname "{}" \; | sort -u); do
     data_name=$(basename $DATA_PATH)
 
     triplanar_nerf_name="triplanar_nerf"-$data_name
@@ -17,10 +20,10 @@ for DATA_PATH in $(find $data_dir -type d -name "*train" -exec dirname "{}" \; |
         echo $triplanar_nerf_name >> $progress_file
     fi
 
-    ngp_nerf_name="npg_nerf"-$data_name
-    if ! grep -q $ngp_nerf_name $progress_file; then
-        echo $ngp_nerf_name
-        # python3 app/main.py --config configs/ngp_nerf.yaml --dataset-path $DATA_PATH --dataset-num-workers 4 --log-dir $checkpoint_dir --exp-name $ngp_nerf_name
-        echo $ngp_nerf_name>> $progress_file
-    fi
+    # ngp_nerf_name="npg_nerf"-$data_name
+    # if ! grep -q $ngp_nerf_name $progress_file; then
+    #     echo $ngp_nerf_name
+    #     # python3 app/main.py --config configs/ngp_nerf.yaml --dataset-path $DATA_PATH --dataset-num-workers 4 --log-dir $checkpoint_dir --exp-name $ngp_nerf_name
+    #     echo $ngp_nerf_name>> $progress_file
+    # fi
 done
